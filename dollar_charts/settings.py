df0f52eae6459a1220a303e4d004844d52f36248
flexocarpius <flexocarpius@gmail.com>
2021-03-09 15:22:55 -0800

    Removed unused directory.

--- dollar_charts/settings.py
+++ dollar_charts/settings.py
@@ -134,10 +134,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    BASE_DIR / "dollar_charts_webapp/static",
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static')
-]
+)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
