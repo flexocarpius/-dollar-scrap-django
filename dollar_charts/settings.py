b4c94902f2816113cfdbf90c63a07587f53fd2ea
flexocarpius <flexocarpius@gmail.com>
2021-04-14 18:04:34 -0700

    Added development urls.

--- dollar_charts/settings.py
+++ dollar_charts/settings.py
@@ -35,7 +35,8 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
     'https://dollar-web-app.herokuapp.com/',
-    'localhost:8100',
+    'http://localhost:8100',
+    'http://localhost',
 ]
 
 
