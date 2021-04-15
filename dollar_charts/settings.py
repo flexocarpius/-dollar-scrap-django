26c4fd134e95d58666e86b0851b20c0023174797
flexocarpius <flexocarpius@gmail.com>
2021-04-15 10:38:11 -0700

    Added custom cors middleware.

--- dollar_charts/settings.py
+++ dollar_charts/settings.py
@@ -66,6 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'dollar_charts_webapp.middleware.corsMiddleware',
 ]
 
 ROOT_URLCONF = 'dollar_charts.urls'
